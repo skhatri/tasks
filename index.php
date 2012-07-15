fbb804004e7478d8533fa99cd6c975693c1d365f
Suresh Khatri <servlet2@msn.com>
2012-07-15 13:43:41 +1000

    menu for the add tasks

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!doctype html>
+<html>
+<head>
+<title>Display Tasks</title>
+<script type="text/javascript" href="js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" href="js/bootstrap.min.js"></script>
+<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+</head>
+<body>
+
+<div class="container">
+
+<div class="alert">
+  <a href="addtask.php" class="btn btn-primary">Add Tasks 1</a>
+  <a href="addtask2.php" class="btn btn-danger">Add Tasks 2</a>
+  <a href="addtask3.php" class="btn btn-info">Add Tasks 3</a>
+</div>
+
+</div>
+
+</body>
+</html
